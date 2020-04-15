476212d774f4dc97a36aea63d0147982ffa1025c
Yuno <yunooooo@users.noreply.github.com>
2020-04-15 00:12:26 -0500

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -282,7 +282,7 @@ function file_video(path){
 	<video class="mdui-video-fluid mdui-center" preload controls>
 	  <source src="${url}" type="video/mp4">
 	</video>
-	<br>${playBtn}
+	<br>
 	<!-Fixed label->
 	<div class="mdui-textfield">
 	  <label class="mdui-textfield-label">download link</label>
