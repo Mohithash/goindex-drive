9c0ba960d42a0d2061bc754039b038f79f7ec934
Yuno <50550119+yunooooo@users.noreply.github.com>
2020-04-28 23:02:41 -0500

    Add files via upload

--- themes/material/app.js
+++ themes/material/app.js
@@ -280,7 +280,7 @@ function file_video(path){
 	var content = `
 <div class="mdui-container-fluid">
 	<br>
-	<video class="mdui-video-fluid mdui-center" preload controls>
+	<video class="mdui-video-fluid mdui-center" preload controls volume="0.5">
 	  <source src="${url}" type="video/mp4">
 	</video>
 	<br>${playBtn}
